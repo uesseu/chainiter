5a30f64e373f9a8dcc210104ebc9127dfda7c4a1
uesseu <sheepwing@kyudai.jp>
2020-06-06 20:25:08 +0900

    setup.py

--- chainiter/__init__.py
+++ chainiter/__init__.py
@@ -1,6 +1,4 @@
 from .chainiter import (ChainIter, run_async, future, ProgressBar,
                         default_progressbar, curry, chain_product)
 __all__: list = []
-__version_info__ = (0, 0, 2)
-__version__ = '.'.join(map(str, __version_info__))
 
